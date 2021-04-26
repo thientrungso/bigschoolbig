b7e82af36e30257553031f6bed1266035220ba7e
= <=>
2021-04-26 21:26:09 +0700

    Test thoi muon oa thi xoa

---
+++ BigSchool/Models/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.Models
+{
+    public class Test //để ké ha muốn xóa thì xóa
+    {
+        public string TestGitc { get; set; }
+    }
+}
